284a95627bcada7aa31f632a3ae05b58693d24e7
Kyle Ellrott <kellrott@gmail.com>
2014-12-17 16:57:51 -0800

    Adding in config file option for run_galaxy_workflow

--- scripts/run_galaxy_workflow.py
+++ scripts/run_galaxy_workflow.py
@@ -16,6 +16,7 @@ import re
 import json
 import time
 import logging
+import yaml
 from urlparse import urlparse
 from glob import glob
 from argparse import ArgumentParser
@@ -29,7 +30,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 def run_workflow(args):
     data_map = {}
-    for meta_path in glob(os.path.join(args.data, "*.json")):
+    for meta_path in glob(os.path.join(args['data'], "*.json")):
         data_path = re.sub(r'.json$', "", meta_path)
         if os.path.exists(data_path):
             try:
@@ -40,13 +41,13 @@ def run_workflow(args):
             except:
                 pass
 
-    o_url = urlparse(args.object_store)
+    o_url = urlparse(args['object_store'])
     if o_url.scheme == '':
-        obj = DiskObjectStore(DiskObjectStoreConfig(job_work=args.local_store, new_file_path=args.local_store), file_path=o_url.path)
+        obj = DiskObjectStore(DiskObjectStoreConfig(job_work=args['local_store'], new_file_path=args['local_store']), file_path=o_url.path)
     else:
         raise Exception("Object Store type not supported: %s" % (o_url.scheme))
 
-    d_url = urlparse(args.doc_store)
+    d_url = urlparse(args['doc_store'])
     if d_url.scheme == '':
         doc = FileDocStore(file_path=d_url.path)
     else:
@@ -55,7 +56,7 @@ def run_workflow(args):
 
     #this side happens on the master node
     tasks = []
-    for i, input_file in enumerate(args.inputs):
+    for i, input_file in enumerate(args['inputs']):
         with open(input_file) as handle:
             meta = json.loads(handle.read())
         inputs = {}
@@ -68,20 +69,20 @@ def run_workflow(args):
                 doc.put(t.uuid, t.to_dict())
             inputs[k] = t
         params = meta.get("parameters", {})
-        if args.workflow is not None:
-            task = GalaxyWorkflow('task_%s' % (i), args.workflow, inputs=inputs, parameters=params, docker=args.galaxy, tool_dir=args.tools, tool_data=args.tool_data)
+        if args['workflow'] is not None:
+            task = GalaxyWorkflow('task_%s' % (i), args['workflow'], inputs=inputs, parameters=params, docker=args['galaxy'], tool_dir=args['tools'], tool_data=args['tool_data'])
         else:
-            with open(args.yaml_workflow) as handle:
+            with open(args['yaml_workflow']) as handle:
                 yaml_text = handle.read()
-            task = GalaxyWorkflow('task_%s' % (i), yaml=yaml_text, inputs=inputs, parameters=params, docker=args.galaxy, tool_dir=args.tools, tool_data=args.tool_data)
+            task = GalaxyWorkflow('task_%s' % (i), yaml=yaml_text, inputs=inputs, parameters=params, docker=args['galaxy'], tool_dir=args['tools'], tool_data=args['tool_data'])
         task_data = task.get_task_data()
         tasks.append(task_data)
 
     #this side happens on the worker node
     service = ServiceFactory('galaxy', objectstore=obj,
-        lib_data=[args.object_store], tool_dir=args.tools, tool_data=args.tool_data,
-        docker_tag=args.galaxy, work_dir=args.warpdrive_dir, sudo=args.sudo, force=True,
-        tool_docker=True)
+        lib_data=[args['object_store']], tool_dir=args['tools'], tool_data=args['tool_data'],
+        docker_tag=args['galaxy'], work_dir=args['warpdrive_dir'], sudo=args['sudo'], force=True,
+        tool_docker=True, smp=args['smp'])
     service.start()
     job_ids = []
     for task_data in tasks:
@@ -109,19 +110,21 @@ def run_workflow(args):
                 service.store_meta(a, doc)
 
     print "Done"
-    if not args.hold:
+    if not args['hold']:
         service.stop()
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-g", "--galaxy", help="Galaxy Runner Image", default="bgruening/galaxy-stable:dev")
-    parser.add_argument("-c", "--warpdrive-dir", help="Directory For Warpdrive config mounting", default="/tmp")
+    parser.add_argument("-wd", "--warpdrive-dir", help="Directory For Warpdrive config mounting", default="/tmp")
     parser.add_argument("-w", "--workflow", help="Galaxy Workflow File")
+    parser.add_argument("-c", "--config", default=None)
     parser.add_argument("-y", "--yaml-workflow", help="Galaxy YAML Workflow File")
     parser.add_argument("-d", "--data", help="Data directory (metadata as .json files)", required=True)
     parser.add_argument("-t", "--tools", help="Tool Directory", required=True)
     parser.add_argument("-td", "--tool-data", help="Tool Directory", default=None)
+    parser.add_argument("--smp", action="append", nargs=2, default=[])
     parser.add_argument("-s", "--object-store", default="./nebula_data")
     parser.add_argument("-b", "--doc-store", default="./nebula_docs")
     parser.add_argument("-l", "--local-store", default="./nebula_work")
@@ -133,4 +136,12 @@ if __name__ == "__main__":
     if args.yaml_workflow is None and args.workflow is None:
         sys.stderr.write("Must define workflow with -w or -y flags")
         sys.exit(1)
-    run_workflow(args)
+    vargs = vars(args)
+    if args.config is not None:
+        with open(args.config) as handle:
+            txt = handle.read()
+        nargs = yaml.load(txt)
+        for k,v in nargs.items():
+            vargs[k] = v
+
+    run_workflow(vargs)
